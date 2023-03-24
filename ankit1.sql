10c7c7e57353f08429040db3fc17ea125ee9a25d
Ankit Makadiya <ankit.makadiya@nexuslinkservices.in>
2023-03-24 17:37:19 +0530

    update

--- ankit1.sql
+++ ankit1.sql
@@ -1,2 +1,3 @@
 UPDATE `messages` SET `deleted_at` = '2023-03-24 17:33:28' WHERE `messages`.`id` = 1;
 UPDATE `pages` SET `deleted_at` = '2023-03-24 17:34:00' WHERE `pages`.`id` = 1;
+UPDATE `plans` SET `deleted_at` = '2023-03-24 17:36:39' WHERE `plans`.`id` = 1;
